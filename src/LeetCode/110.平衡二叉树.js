36c85690017d22efcee05075f578d27ba9043ded
Hank.Ren <987942918@qq.com>
2019-11-02 13:41:02 +0800

    style: test lint-stged

--- src/LeetCode/110.平衡二叉树.js
+++ src/LeetCode/110.平衡二叉树.js
@@ -41,10 +41,11 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
+var isBalanced = function (root) {
   if (!root) return true;
   function maxDepth(root) {
     return root === null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
   }
-  return Math.abs(maxDepth(root.left) - maxDepth(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+  return Math.abs(
+    maxDepth(root.left) - maxDepth(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
 };
