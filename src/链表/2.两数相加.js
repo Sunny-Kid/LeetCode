4587fc5e67a1ab9d94cb704de13b689e8be7833b
hank_mj <987942918@qq.com>
2023-07-18 11:39:18 +0800

    feat: add string

--- src/链表/2.两数相加.js
+++ src/链表/2.两数相加.js
@@ -35,8 +35,10 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  let head = null, tail = null;
+  let head = null;
+  let tail = null;
   let carry = 0;
+
   while (l1 || l2) {
     const n1 = l1 ? l1.val : 0;
     const n2 = l2 ? l2.val : 0;
@@ -44,8 +46,7 @@ var addTwoNumbers = function (l1, l2) {
     if (!head) {
       head = tail = new ListNode(sum % 10);
     } else {
-      tail.next = new ListNode(sum % 10);
-      tail = tail.next;
+      tail = tail.next = new ListNode(sum % 10);
     }
     carry = Math.floor(sum / 10);
     if (l1) {
